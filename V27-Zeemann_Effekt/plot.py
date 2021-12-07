af23244ca22bbd9f7612a78ebb9b2ae794f49e16
Konstantin Mrozik <konstantin.mrozik@udo.edu>
2021-12-07 20:16:25 +0100

    Rechnungen für V27 fertig

--- V27-Zeemann_Effekt/plot.py
+++ V27-Zeemann_Effekt/plot.py
@@ -73,3 +73,85 @@ plt.legend(loc = 'best')
 plt.tight_layout()
 plt.savefig("plots/B_Feld.pdf")
 plt.close()
+
+
+
+df1 = pd.read_csv('data/data_test_rot_643.txt'
+                 ,header =[1])
+df2 = pd.read_csv('data/data_test_blau.txt'
+                 ,header =[1])
+
+
+def delta_lambda(lam,n,d):
+    """Caculates the Dispersion area from the thickness, the wavelength and the difraction index
+
+        Parameters
+        ----------
+        lam: float
+            The wavelength lambda in meters
+        n: float
+            The diffraction index of the material
+        d: float
+            The thickness of the material in meters.
+
+        Returns
+        -------
+        delta_lambda: float
+            The Disperison region for the material with the specified wavelength.
+    """
+    delta_lambda = lam**2/(2*d)*np.sqrt(1/(n**2-1))
+    return delta_lambda
+
+def add_del_lambda(df,lam,n):
+    """Adds the Dispersion region to the given data frame.
+
+        Parameters
+        ----------
+        df: pandas data frame
+            The data frame with the information.
+        lam: float
+            The wavelength lambda in meters
+        n: float
+            The diffraction index of the material
+            
+
+        Returns
+        -------
+        df: pandas data frame
+            Data Frame with the added Disperion region
+    """
+    df['del_lambda'] = 0.5*(df['del_s']/df['delta_s'])*delta_lambda(lam,n,0.004)
+    df['del_lambda_err'] = 0.5*df['del_lambda']*np.sqrt((3/df['delta_s'])**2+(3/df['del_s'])**2)
+    return df
+
+def get_landre(del_lam,B,lam):
+    """Adds the Dispersion region to the given data frame.
+
+        Parameters
+        ----------
+        del_lam: float
+            The wavelength difference in meters
+        lam: float
+            The wavelength lambda in meters
+        B: float
+            The magnetic field strength in Tesla
+            
+
+        Returns
+        -------
+        g: float
+            The Landre Factor
+    """
+    g = del_lam*(const.h*const.c)/(const.physical_constants['Bohr magneton'][0]*B*lam**2)
+    return g
+
+df1 = add_del_lambda(df1,6.438*10**(-7),1.4567)
+del_lambda1 = df1['del_lambda'].mean(axis = 0)
+g1 = get_landre(del_lambda1,0.6194,6.438*10**(-7))
+
+df2 = add_del_lambda(df2,4.8*10**(-7),1.4635)
+del_lambda2 = df2['del_lambda'].mean(axis = 0)
+g2 = get_landre(del_lambda2,1.0594 ,4.8*10**(-7))
+
+print(f"Für das rote Licht: g = {g1}")
+print(f"Für das blaue Licht: g = {g2}")
